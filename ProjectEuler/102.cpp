699e1d63f0e9c13b44e1c5ebfef1bf1a014b82b3
tusikalanse <615822545@qq.com>
2021-08-12 20:53:52 +0800

    PE102  judge point in triangle

--- ProjectEuler/102.cpp
+++ ProjectEuler/102.cpp
@@ -1,9 +1,9 @@
 /*
  * @Author: tusikalanse
  * @Date: 2021-08-12 20:31:47
- * @LastEditTime: 2021-08-12 20:32:16
+ * @LastEditTime: 2021-08-12 20:49:19
  * @LastEditors: tusikalanse
- * @Description:
+ * @Description: judge point in triangle
  */
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,69 +16,22 @@ const LL INFL = 0x3f3f3f3f3f3f3f3f;
 const int mod = 1e9 + 7;
 //-------------------end head--------------
 
-vector<int> todo{1, 4, 9, 16, 25, 36, 49, 64, 81};
-int ans = 0;
-int vis[2][10];
-int a[2][10];
+struct Point {
+    int x, y;
+};
 
-bool gao() {
-    for (int x : todo) {
-        bool f = false;
-        for (int i = 0; i < 6; ++i) {
-            for (int j = 0; j < 6; ++j) {
-                if (a[0][i] * 10 + a[1][j] == x || a[1][j] * 10 + a[0][i] == x)
-                    f = true;
-                if (a[0][i] == 9) {
-                    a[0][i] = 6;
-                    if (a[0][i] * 10 + a[1][j] == x || a[1][j] * 10 + a[0][i] == x)
-                        f = true;
-                    a[0][i] = 9;
-                }
-                if (a[0][i] == 6) {
-                    a[0][i] = 9;
-                    if (a[0][i] * 10 + a[1][j] == x || a[1][j] * 10 + a[0][i] == x)
-                        f = true;
-                    a[0][i] = 6;
-                }
-                if (a[1][j] == 9) {
-                    a[1][j] = 6;
-                    if (a[0][i] * 10 + a[1][j] == x || a[1][j] * 10 + a[0][i] == x)
-                        f = true;
-                    a[1][j] = 9;
-                }
-                if (a[1][j] == 6) {
-                    a[1][j] = 9;
-                    if (a[0][i] * 10 + a[1][j] == x || a[1][j] * 10 + a[0][i] == x)
-                        f = true;
-                    a[1][j] = 6;
-                }
-            }
-        }
-        if (!f)
-            return false;
-    }
-    return true;
-}
-
-void dfs(int now, int pos, int p) {
-    if (now == 6 && p == 1) {
-        ans += gao();
-        return;
-    }
-    if (now == 6) {
-        dfs(0, 0, 1);
-        return;
-    }
-    for (int i = pos; i < 10; ++i) {
-        vis[p][i] = 1;
-        a[p][now] = i;
-        dfs(now + 1, i + 1, p);
-        vis[p][i] = 0;
-    }
+bool Cross_product(Point p0, Point p1, Point p2) {
+    return (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y) >= 0;
 }
 
 int main() {
-    dfs(0, 0, 0);
-    cout << ans / 2 << endl;
+    Point d1, d2, d3, a;
+    a.x = a.y = 0;
+    int ans = 0;
+    while (cin >> d1.x >> d1.y >> d2.x >> d2.y >> d3.x >> d3.y) {
+        bool f1 = Cross_product(d1, d2, a), f2 = Cross_product(d2, d3, a), f3 = Cross_product(d3, d1, a);
+        ans += f1 == f2 && f2 == f3;
+    }
+    cout << ans << endl;
     return 0;
 }
