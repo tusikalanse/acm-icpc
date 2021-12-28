20150f42752b85e286d812e716bb32ae1fa3db70
tusikalanse <615822545@qq.com>
2021-12-28 15:33:07 +0800

    PE 75/139 pythagorean triple

--- ProjectEuler/139.cpp
+++ ProjectEuler/139.cpp
@@ -1,11 +1,11 @@
 /*
  * @Author: tusikalanse
- * @Date: 2021-07-15 19:48:55
- * @LastEditTime: 2021-12-28 15:22:23
+ * @Date: 2021-12-28 15:22:23
+ * @LastEditTime: 2021-12-28 15:32:23
  * @LastEditors: tusikalanse
  * @Description: pythagorean triple
  */
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
 using LL = long long;
 LL gcd(LL a, LL b) {
@@ -16,29 +16,27 @@ const LL INFL = 0x3f3f3f3f3f3f3f3f;
 const int mod = 1e9 + 7;
 //-------------------end head--------------
 
-const int N = 15e5;
-
-map<int, int> cnt;
+const int N = 1e8;
 
 // a = m * m - n * n 
 // b = 2 * m * n
 // c = m * m + n * n
 
 int main() {
+    int ans = 0;
     for (int m = 1; m * m * 2 < N; ++m) {
         for (int n = (m & 1) + 1; n < m && 2 * m * (m + n) <= N; n += 2) {
             if (gcd(n, m) != 1)
                 continue;
             // cout << m * m - n * n << " " << 2 * m * n << " " << m * m + n * n << endl;
             int s = 2 * m * (m + n);
-            for (int i = 1; i * s <= N; ++i)
-                cnt[i * s]++;
+            if ((m * m + n * n) % abs(m * m - n * n - 2 * m * n) == 0) {
+                for (int i = 1; i * s <= N; ++i) {
+                    ans++;
+                }
+            }
         }
     }
-    int ans = 0;
-    for (auto [x, y] : cnt)
-        ans += y == 1;
     cout << ans << endl;
-
     return 0;
 }
